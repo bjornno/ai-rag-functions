4ef9833d22bf7dfb069cebfe9dbdcdd03e1e7bbc
Bjørn Nordlund <bnordlun@cisco.com>
2024-10-29 15:10:09 +0100

    more functions to chose from

--- src/main/kotlin/com/bjornno/ai_rag_functions/Functions.kt
+++ src/main/kotlin/com/bjornno/ai_rag_functions/Functions.kt
@@ -5,30 +5,51 @@ import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonPropertyDescription
 import org.slf4j.LoggerFactory
+import org.springframework.ai.chat.client.ChatClient
 import org.springframework.context.annotation.Description
+import org.springframework.stereotype.Controller
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @JsonClassDescription("Request for the example function")
 data class RequestJokeTheme @JsonCreator
 constructor (
-    @JsonProperty @JsonPropertyDescription("Theme for the joke") val content: String = "test"
+    @JsonProperty @JsonPropertyDescription("Theme for the joke") val requestedTheme: String = "test"
 )
 
-@JsonClassDescription("Request for the captain")
-data class RequestBoatCaptain @JsonCreator
+@JsonClassDescription("Request for the driver")
+data class RequestDriver @JsonCreator
 constructor (
-    @JsonProperty @JsonPropertyDescription("Theme for the joke") val name: String = "Johan"
+    @JsonProperty @JsonPropertyDescription("Theme for the joke") val themeName: String = "cars"
 )
-
 
 data class Response(val content: String)
 
 @Service("boatDriverResolver")
 @Description("When we have a theme for the joke, we also need to know the name of the main character")
-class BoatDriverResolver: java.util.function.Function<RequestBoatCaptain, Response> {
-    override fun apply(t: RequestBoatCaptain): Response {
+class BoatDriverResolver: java.util.function.Function<RequestDriver, Response> {
+    override fun apply(t: RequestDriver): Response {
         LoggerFactory.getLogger(BoatDriverResolver::class.java).info("Boat captain resolver called")
-        return Response("The boat captains name is Kong Harald")
+        return Response("The boat captains name is Jack Sparrow")
+    }
+}
+
+@Service("carDriverResolver")
+@Description("When we have a theme for the joke, we also need to know the name of the main character")
+class CarDriverResolver: java.util.function.Function<RequestDriver, Response> {
+    override fun apply(t: RequestDriver): Response {
+        LoggerFactory.getLogger(CarDriverResolver::class.java).info("Car driver resolver called")
+        return Response("The car driver is called Michael Schumacher")
+    }
+}
+@Service("motorcycleDriverResolver")
+@Description("When we have a theme for the joke, we also need to know the name of the main character")
+class MotorcycleDriverResolver: java.util.function.Function<RequestDriver, Response> {
+    override fun apply(t: RequestDriver): Response {
+        LoggerFactory.getLogger(MotorcycleDriverResolver::class.java).info("Motorcycle driver resolver called")
+        return Response("The motorcycle driver is called Valentino Rossi")
     }
 }
 
@@ -36,7 +57,31 @@ class BoatDriverResolver: java.util.function.Function<RequestBoatCaptain, Respon
 @Description("Joke theme resolver service. This service will provide a theme for the joke")
 class JokeThemeResolver: java.util.function.Function<RequestJokeTheme, Response> {
     override fun apply(t: RequestJokeTheme): Response {
-        LoggerFactory.getLogger(BoatDriverResolver::class.java).info("Joke theme resolver called")
-        return Response("The joke should be about a boat. But I also need to know the name of the boat captain")
+        LoggerFactory.getLogger(JokeThemeResolver::class.java).info("Joke theme resolver called with wanted theme ${t.requestedTheme}")
+        return when(t.requestedTheme) {
+            "boat" -> Response("The joke should be about a boat, but I also need to know the name of the boat captain")
+            "car" -> Response("The joke should be about a car, but I also need to know the name of the car driver")
+            "motorcycle" -> Response("The joke should be about a motorcycle, but I also need to know the name of the motorcycle driver")
+            else -> Response("The joke should be about a boat, but I also need to know the name of the boat captain")
+        }
+    }
+}
+
+fun interface Jokey: (String) -> String
+
+@Controller
+class Controller(val builder: ChatClient.Builder): Jokey {
+    @GetMapping("/")
+    @ResponseBody
+    override fun invoke(@RequestParam wantedTheme: String): String {
+        val chatClient: ChatClient = builder.build()
+        val prompt = "Tell me a joke based on $wantedTheme."
+        val logger = LoggerFactory.getLogger(Controller::class.java)
+        logger.info("Sending prompt: $prompt")
+        val response: String = chatClient.prompt(prompt)
+            .functions("jokeThemeResolver", "boatDriverResolver", "carDriverResolver", "motorcycleDriverResolver")
+            .call().content()
+        logger.info("Received response: $response")
+        return response
     }
 }
